6e5b22e77dc24cd05115e60a13758588af6f852f
Eugene Dorfman <eugene.dorfman@gmail.com>
2019-07-01 20:25:33 +0200

    sourcing py3.7 virtualenv

--- upload.sh
+++ upload.sh
@@ -1,4 +1,4 @@
-source ~/.virtualenvs/python3.6.3/bin/activate
+source ~/.virtualenvs/py3.7/bin/activate
 script_dir=$( dirname "${BASH_SOURCE[0]}" )
 echo $script_dir
 python $script_dir/upload.py $1 $2
